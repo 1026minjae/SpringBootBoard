4499d18dd65dd7646fbce93b690fca4ae2866773
1026minjae <minjae2k1@gmail.com>
2025-06-22 15:52:40 +0900

    DevTools added. 2-02 done.

---
+++ sbb_kotlin/src/main/kotlin/com/sbb/sbb_kotlin/controller/HelloControler.kt
@@ -0,0 +1,14 @@
+package com.sbb.sbb_kotlin.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    fun hello(): String {
+        return "Hello World!"
+    }    
+}
