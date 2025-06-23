65d0b7e895d6bfb0e6c4dc40708b02bca9996067
1026minjae <minjae2k1@gmail.com>
2025-06-23 16:46:08 +0900

    MySQL Connected

---
+++ sbb_kotlin/src/main/kotlin/com/sbb/sbb_kotlin/MySqlConnectionTester.kt
@@ -0,0 +1,16 @@
+package com.sbb.sbb_kotlin
+
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import org.springframework.boot.context.event.ApplicationReadyEvent
+
+@Component
+class DbTest(private val jdbcTemplate: JdbcTemplate) {
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun testConnection() {
+        val version: String? = jdbcTemplate.queryForObject("SELECT VERSION()", String::class.java)
+        println("MySQL version: $version")
+    }
+}
