d7ca9afffcd8de64c9aa8c8687f1a4dbd973d504
1026minjae <minjae2k1@gmail.com>
2025-07-02 22:52:04 +0900

    2-11 done

--- sbb_kotlin/src/main/kotlin/com/sbb/sbb_kotlin/question/QuestionController.kt
+++ sbb_kotlin/src/main/kotlin/com/sbb/sbb_kotlin/question/QuestionController.kt
@@ -4,23 +4,25 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 
 import com.sbb.sbb_kotlin.question.QuestionRepository
 import com.sbb.sbb_kotlin.question.QuestionService
 
+@RequestMapping("/question")
 @Controller
 class QuestionController (
     private val questionRepo: QuestionRepository,
     private val questionService: QuestionService
 ) {
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     fun list(model: Model): String {
         val questionList = questionService.getList()
         model.addAttribute("questionList", questionList)
         return "question_list"
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     fun detail(model: Model, @PathVariable("id") id: Long): String {
         val question = questionService.getQuestion(id)
         model.addAttribute("question", question)
